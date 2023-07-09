b94921d6d0a34a6dab1ab04e210b3bd5a5f68cbf
seoyeong643 <105833016+seoyeong643@users.noreply.github.com>
2023-07-09 13:27:09 -0700

    Update Patient.java - removed pharma1 and pharma2

--- Patient.java
+++ Patient.java
@@ -1,23 +1,13 @@
-package application;
+package com.example.cse360project;
 
 public class Patient {
-public String firstName;
-public String lastName;
-public String dateOfBirth;
-public String pharma1;
-public String pharma2;
-public Patient(String fName, String lName, String dob)
-{
-	firstName = fName;
-	lastName = lName;
-	dateOfBirth = dob;
-}
-public void setPharma1(String newP1)
-{
-	pharma1 = newP1;
-}
-public void setPharma2(String newP2)
-{
-	pharma2 = newP2;
-}
+    public String firstName;
+    public String lastName;
+    public String dateOfBirth;
+    public Patient(String fName, String lName, String dob)
+    {
+        firstName = fName;
+        lastName = lName;
+        dateOfBirth = dob;
+    }
 }
